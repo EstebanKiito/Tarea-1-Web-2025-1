80545a8a0f152e1c9da3b32f30a9754ef71e9058
EstebanKiito <esteban.ortega@uc.cl>
2025-07-22 00:52:49 -0400

    Finished Login Css

--- tarea-1-2025-1/src/pages/Login.jsx
+++ tarea-1-2025-1/src/pages/Login.jsx
@@ -4,9 +4,34 @@
  * Para efectos prácticos, en esta tarea no se considerará el manejo
  * de crear nuevos usuarios.
  */
-
+import styles from "./Login.module.css"; // Asegúrate de tener un archivo CSS para estilos
 function Login() {
-  return <div>Login</div>;
+  return (
+    <>
+      <div className={styles.login_container}>
+        <form>
+          <h2>Iniciar Sesión</h2>
+          <label htmlFor="username"></label>
+          <input
+            type="text"
+            id="username"
+            name="username"
+            placeholder="Nombre de usuario"
+            required
+          />
+          <label htmlFor="password"></label>
+          <input
+            typo="password"
+            id="password"
+            name="password"
+            placeholder="Contraseña"
+            required
+          />
+          <button type="submit">Entrar</button>
+        </form>
+      </div>
+    </>
+  );
 }
 
 export default Login;
