2ab74174d5e7ab4139b6c693642ed4d662c09f32
EstebanKiito <esteban.ortega@uc.cl>
2025-07-24 12:17:25 -0400

    Listo base Posts: Posts de la Api y un poco de css

--- tarea-1-2025-1/src/pages/Posts.jsx
+++ tarea-1-2025-1/src/pages/Posts.jsx
@@ -5,11 +5,65 @@
  * Además, debe tener un botón que permita que los usuarios agreguen un nuevo post.
  */
 
-function Posts(isLoggedIn) {
+import { useState, useEffect } from "react";
+import styles from "./Posts.module.css"; // Asegúrate de tener un archivo CSS para estilos
+import axios from "axios";
+
+function Posts() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [posts, setPosts] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+  const url = "https://dummyjson.com/posts";
+
+  const fetchPosts = async () => {
+    try {
+      const response = await axios.get(url);
+      setPosts(response.data.posts);
+      setLoading(false);
+      console.log("Posts fetched:", response.data.posts);
+    } catch (err) {
+      setError("Error al cargar los posts. Intenta nuevamente.");
+      console.error("Error al cargar los posts:", err);
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    // Simulación de verificación de sesión
+    const session = localStorage.getItem("isLoggedIn");
+    setIsLoggedIn(session === "true");
+    if (session === "true") {
+      fetchPosts();
+    }
+  }, []);
+
+  const fakePosts = [
+    { id: 1, title: "Post 1", content: "Contenido del post 1" },
+  ];
+
   return (
-    <div>
-      <h1>Posts</h1>
-      {isLoggedIn && <button>Agregar nuevo post</button>}
+    <div className={styles.post_container}>
+      <div className={styles.post_add}>
+        {isLoggedIn && (
+          <button className={styles.add_button}>Agregar nuevo post</button>
+        )}
+      </div>
+      <div className={styles.post_list}>
+        <ul>
+          {posts.map((post) => (
+            <div className={styles.posts}>
+              <li key={post.id}>
+                <p className={styles.post_title}>{post.title}</p>
+                <p>{post.body}</p>
+                <p>
+                  <strong>Autor:</strong> {post.userId}
+                </p>
+              </li>
+            </div>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
