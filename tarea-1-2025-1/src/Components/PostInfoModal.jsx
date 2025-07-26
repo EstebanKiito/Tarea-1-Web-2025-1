33c11035219c38f9f2fc7a7f1092d06010a39426
EstebanKiito <esteban.ortega@uc.cl>
2025-07-25 22:15:42 -0400

    Finished Post Info Modal

--- tarea-1-2025-1/src/Components/PostInfoModal.jsx
+++ tarea-1-2025-1/src/Components/PostInfoModal.jsx
@@ -4,26 +4,29 @@
  * Junto con ello,
  * destacar que el bonus de la tarea considera
  * mostrar los comentarios de un post en este componente. */
+import styles from "./PostInfoModal.module.css"; // Asegúrate de tener un archivo CSS para estilos
 
 function PostInfoModal({ post, onClose, author }) {
   return (
-    <div className="modal" onClick={onClose}>
-      <ul>
-        <li key={post.id}>
-          <strong>Autor:</strong> {author}
-          <p>{post.title}</p>
-          <p>{post.body}</p>
-          <p style={{ color: "#6d76af" }}>
-            #{post.tags.map((tag) => ` ${tag} `)}
-          </p>
-          <hr />
-          <footer>
-            <p>{post.reactions.likes} 👍</p>
-            <p>{post.reactions.dislikes} 👎</p>
-            <p>{post.views} 👀</p>
-          </footer>
-        </li>
-      </ul>
+    <div className={styles.modal_container}>
+      <div className={styles.modal} onClick={onClose}>
+        <ul>
+          <li key={post.id}>
+            <strong>Autor:</strong> {author}
+            <p className={styles.post_title}>{post.title}</p>
+            <p>{post.body}</p>
+            <p style={{ color: "#6d76af" }}>
+              #{post.tags.map((tag) => ` ${tag} `)}
+            </p>
+            <hr />
+            <footer>
+              <p>{post.reactions.likes} 👍</p>
+              <p>{post.reactions.dislikes} 👎</p>
+              <p>{post.views} 👀</p>
+            </footer>
+          </li>
+        </ul>
+      </div>
     </div>
   );
 }
